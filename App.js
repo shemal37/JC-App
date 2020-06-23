2c05112c895bce7856b03b994bc22b970b4af9e3
Shemal Shah <shemal.shah07@gmail.com>
2020-06-23 15:25:42 -0400

    HomePage Skeleton created

--- App.js
+++ App.js
@@ -1,36 +1,26 @@
-import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
-import * as React from 'react';
-import { Platform, StatusBar, StyleSheet, View } from 'react-native';
-
-import useCachedResources from './hooks/useCachedResources';
-import BottomTabNavigator from './navigation/BottomTabNavigator';
-import LinkingConfiguration from './navigation/LinkingConfiguration';
+import * as React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import HomeScreen from "./screens/HomeScreen";
+import BusinessScreen from "./screens/BusinessScreen";
+import JobsScreen from "./screens/JobsScreen";
+import MembersScreen from "./screens/MembersScreen";
+import EventsScreen from "./screens/EventsScreen";
 
 const Stack = createStackNavigator();
 
-export default function App(props) {
-  const isLoadingComplete = useCachedResources();
-
-  if (!isLoadingComplete) {
-    return null;
-  } else {
-    return (
-      <View style={styles.container}>
-        {Platform.OS === 'ios' && <StatusBar barStyle="dark-content" />}
-        <NavigationContainer linking={LinkingConfiguration}>
-          <Stack.Navigator>
-            <Stack.Screen name="Root" component={BottomTabNavigator} />
-          </Stack.Navigator>
-        </NavigationContainer>
-      </View>
-    );
-  }
+function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Home">
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="Jobs" component={JobsScreen} />
+        <Stack.Screen name="Events" component={EventsScreen} />
+        <Stack.Screen name="Members" component={MembersScreen} />
+        <Stack.Screen name="Business" component={BusinessScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-  },
-});
+export default App;
---
+++ screens/MembersScreen.js
@@ -0,0 +1,18 @@
+import { StyleSheet, TouchableOpacity, View, Text } from "react-native";
+import { ScrollView } from "react-native-gesture-handler";
+import * as React from "react";
+
+export default function MemberScreen() {
+  return (
+    <View style={styles.container}>
+      <Text>Hello from the Members Screen</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+  },
+});
